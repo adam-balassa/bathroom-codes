7117a8d7cbcb79fd587ccc19a5f4d3fa97e1f3e9
Adam Balassa <balassaadi@gmail.com>
2020-06-09 11:35:56 +0200

    init

---
+++ src/main/kotlin/hu/badam/bathroomcodes/BathroomcodesApplication.kt
@@ -0,0 +1,11 @@
+package hu.badam.bathroomcodes
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BathroomcodesApplication
+
+fun main(args: Array<String>) {
+	runApplication<BathroomcodesApplication>(*args)
+}
---
+++ src/test/kotlin/hu/badam/bathroomcodes/BathroomcodesApplicationTests.kt
@@ -0,0 +1,13 @@
+package hu.badam.bathroomcodes
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BathroomcodesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
